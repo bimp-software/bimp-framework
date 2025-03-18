206d42ea6218e6a3cb67b46f1d7fc55262bfcb7d
bimp-software <bimp.software@gmail.com>
2025-03-18 00:54:52 -0300

    Se modifico el navbar del home

--- resources/components/navbar.php
+++ resources/components/navbar.php
@@ -1,40 +1,41 @@
-<nav class="navbar navbar-expand-lg navbar-bimp sticky-top">
+<nav class="navbar navbar-expand-lg navbar-bimp bg-body-tertiary sticky-top navbar-light">
     <div class="container">
+        <!-- Logo y Nombre -->
         <a class="navbar-brand d-flex align-items-center" href="#">
             <img src="<?php echo FAVICON ?>icono-bimp.png" alt="Bimp Framework" width="40" height="40" class="me-2">
             <span class="fw-bold text-dark">Bimp Framework</span>
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+
+        <!-- Botón de menú hamburguesa -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+
+        <!-- Menú de navegación -->
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav me-auto">
+            <ul class="navbar-nav mx-auto text-lg-start text-center">
                 <li class="nav-item">
-                    <a class="nav-link" href="#" role="button" aria-expanded="false">
-                        Productos
-                    </a>
+                    <a class="nav-link" href="#">Productos</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#" role="button" aria-expanded="false">
-                        Open Source
-                    </a>
+                    <a class="nav-link" href="#">Open Source</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#" role="button" aria-expanded="false">
-                        Desarrolladores
-                    </a>
+                    <a class="nav-link" href="#">Desarrolladores</a>
                 </li>
             </ul>
-            <div class="d-flex align-items-center">
 
+            <!-- Sección derecha del Navbar -->
+            <div class="d-flex flex-column flex-lg-row align-items-center text-center text-lg-start">
                 <!-- Contador de descargas con ícono de GitHub -->
-                <div class="d-flex align-items-center text-dark me-3">
+                <div class="d-flex align-items-center text-dark me-lg-3 justify-content-center mb-2 mb-lg-0">
                     <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub" width="20" height="20" class="me-1">
                     <span class="fw-bold">79K</span>
                 </div>
 
-                <a href="#" class="btn btn-outline-bimp me-3">Documentación</a>
-
+                <!-- Botón de documentación -->
+                <a href="#" class="btn btn-outline-bimp w-100 w-lg-auto">Documentación</a>
             </div>
         </div>
     </div>
